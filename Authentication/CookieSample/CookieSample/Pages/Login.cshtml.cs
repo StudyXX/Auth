297c60fd07dc737a839f43c6411239609b9b4c67
guodf <guo_net_win@live.com>
2018-09-25 11:11:44 +0800

    详解Microsoft.AspNetCore.Authentication.Cookies Demo

---
+++ Authentication/CookieSample/CookieSample/Pages/Login.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CookieSample.Pages
+{
+    public class LoginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+
+        public void OnPost()
+        {
+        }
+    }
+}
